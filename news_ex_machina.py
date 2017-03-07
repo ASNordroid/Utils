d3ba7d9aa7c06f1d5538e63252acbf7f7fc7c38a
Ant <1>
2017-03-08 02:57:08 +0300

    Now saving first link to file to have access to it after restart. Deleted 'utf-8' in encode and decode functions, since it's standard argument

--- news_ex_machina.py
+++ news_ex_machina.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 
 def learn_my_taste():
     with open('C:\\Users\\Ant\\Desktop\\best.txt', 'rb') as my_taste_file:
-        my_taste = my_taste_file.read().decode('utf-8')
+        my_taste = my_taste_file.read().decode()
     sentences = my_taste.split('\n')
 
     words = []
@@ -33,24 +33,32 @@ def get_news():
     next_page = 'https://news.ycombinator.com/newest'
     news_str = []
     junk = ["from?", "item?"]
-    i = 0
-    while next_page != '':
+    l = 0 # temp, for testing
+    save_page = ''
+
+    while l < 3:
+        l += 1
         links = []
         news_html = rq.get(next_page).content
         soup = BeautifulSoup(news_html, "html.parser")
 
         for tag in soup.find_all("td", {"class": "title"}):
-            t = tag.find_all("a")
-            for tg in t:
+            for tg in tag.find_all("a"):
                 if all(_ not in tg['href'] for _ in junk):
                     links.append(tg.string + ' - ' + str(tg['href']) + '\n')
 
-        if i == 0:
-            i += 1
-            save_page = news_str[0][0]
+        if next_page == 'https://news.ycombinator.com/newest':
+            print(links[0])
+            save_page = links[0]
         else:
-            for i in range(1, len(news_str)):
-                if save_page in news_str[i]:
+            with open('C:\\Users\\Ant\\Desktop\\news.txt', '+b') as news_file:
+                temp = news_file.read().decode()
+                print('tmp' + temp[0])
+                load_page = temp[0]
+                news_file.write(temp[1:].encode())
+            for i in links:
+                if load_page == i:
+                    print('broke')
                     break
 
         next_page = 'https://news.ycombinator.com/' + links[-1].split(' - ')[1]
@@ -60,10 +68,14 @@ def get_news():
     #     for j in i:
     #         print(j)
 
-    with open('C:\\Users\\Ant\\Desktop\\news.txt', 'ab') as news_file:
+    with open('C:\\Users\\Ant\\Desktop\\news.txt', 'rb') as news_file:
+        temp = news_file.read().decode()
+    with open('C:\\Users\\Ant\\Desktop\\news.txt', 'wb') as news_file:
+        save_page += temp
+        news_file.write(save_page.encode())
         for i in news_str:
             for j in i:
-                news_file.write(j.encode('utf-8'))
+                news_file.write(j.encode())
 
 get_news()
 learn_my_taste()
