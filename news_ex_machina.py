dc745958bfaaa694fb437182485ce6ba054e5a35
Ant <1>
2017-03-08 03:37:49 +0300

    Temporaly disabled saving

--- news_ex_machina.py
+++ news_ex_machina.py
@@ -33,7 +33,7 @@ def get_news():
     next_page = 'https://news.ycombinator.com/newest'
     news_str = []
     junk = ["from?", "item?"]
-    l = 0 # temp, for testing
+    l = 0  # temp, for testing
     save_page = ''
 
     while l < 3:
@@ -47,35 +47,35 @@ def get_news():
                 if all(_ not in tg['href'] for _ in junk):
                     links.append(tg.string + ' - ' + str(tg['href']) + '\n')
 
-        if next_page == 'https://news.ycombinator.com/newest':
-            print(links[0])
-            save_page = links[0]
-        else:
-            with open('C:\\Users\\Ant\\Desktop\\news.txt', '+b') as news_file:
-                temp = news_file.read().decode()
-                print('tmp' + temp[0])
-                load_page = temp[0]
-                news_file.write(temp[1:].encode())
-            for i in links:
-                if load_page == i:
-                    print('broke')
-                    break
+        # if next_page == 'https://news.ycombinator.com/newest':
+        #     print(links[0])
+        #     save_page = links[0]
+        # else:
+        #     with open('C:\\Users\\Ant\\Desktop\\news.txt', '+b') as news_file:
+        #         temp = news_file.read().decode()
+        #         print('tmp' + temp[0])
+        #         load_page = temp[0]
+        #         news_file.write(temp[1:].encode())
+        #     for i in links:
+        #         if load_page == i:
+        #             print('broke')
+        #             break
 
         next_page = 'https://news.ycombinator.com/' + links[-1].split(' - ')[1]
         news_str.append(links[:-1])
-        # print(next_page)
-    # for i in news_str:
-    #     for j in i:
-    #         print(j)
+        print(next_page)
+    for i in news_str:
+        for j in i:
+            print(j)
 
-    with open('C:\\Users\\Ant\\Desktop\\news.txt', 'rb') as news_file:
-        temp = news_file.read().decode()
-    with open('C:\\Users\\Ant\\Desktop\\news.txt', 'wb') as news_file:
-        save_page += temp
-        news_file.write(save_page.encode())
-        for i in news_str:
-            for j in i:
-                news_file.write(j.encode())
+    # with open('C:\\Users\\Ant\\Desktop\\news.txt', 'rb') as news_file:
+    #     temp = news_file.read().decode()
+    # with open('C:\\Users\\Ant\\Desktop\\news.txt', 'wb') as news_file:
+    #     save_page += temp
+    #     news_file.write(save_page.encode())
+    #     for i in news_str:
+    #         for j in i:
+    #             news_file.write(j.encode())
 
 get_news()
 learn_my_taste()
