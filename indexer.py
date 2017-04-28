28f30979be6f179ff012991de4946f4db0d9718f
Ant <1>
2017-04-29 02:30:24 +0300

    Add get_index function

--- indexer.py
+++ indexer.py
@@ -1,12 +1,18 @@
+from collections import defaultdict
+
 import database as db
 import nlp
 
-# TODO: get all words
-# TODO: for every word find
 
-
-def get_all_words():
+def get_index():
+    index = defaultdict(list)
     headers = db.get_headers()
+    print(headers)
 
     for header in headers:
-        header_words = nlp.normalize(header)
+        header_words = nlp.normalize(header[1])
+
+        for word in header_words:
+            index[word].append(header[0])
+
+    return index
