a0ac90255be8c03b36896eef0789b2fa7038f4ac
Ant <1>
2017-04-28 04:01:07 +0300

    Start working on indexes

--- indexer.py
+++ indexer.py
@@ -1,4 +1,12 @@
-import crawler
+import database as db
+import nlp
 
 # TODO: get all words
 # TODO: for every word find
+
+
+def get_all_words():
+    headers = db.get_headers()
+
+    for header in headers:
+        header_words = nlp.normalize(header)
