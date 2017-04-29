92ba82b3e085983e70fd99ec09fd6a49bfc6b5f7
Ant <1>
2017-04-29 04:43:14 +0300

    Add add_to_index and get_index functions

--- indexer.py
+++ indexer.py
@@ -4,15 +4,28 @@ import database as db
 import nlp
 
 
-def get_index():
+def add_to_index(headers):
     index = defaultdict(list)
-    headers = db.get_headers()
-    print(headers)
 
     for header in headers:
-        header_words = nlp.normalize(header[1])
+        header_words = nlp.normalize(header.split(' ^ ')[1])
 
         for word in header_words:
-            index[word].append(header[0])
+            index[word].append(header.split(' ^ ')[0])
+
+    index_lst = []
+    for x in index:
+        index_lst.append(x + ' ^ ' + ' '.join(index[x]) + '\n')
+
+    db.write_to_base(db.index_db, index_lst)
+
+
+def get_index():
+    index = {}
+    raw = db.read_from_base(db.index_db)
+
+    for i in raw:
+        j = i.split(' ^ ')
+        index[j[0]] = j[1].split()
 
     return index
